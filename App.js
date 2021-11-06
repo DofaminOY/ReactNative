812e26ac55d0bd67687ac779c373bf5dd20c03e0
Yura Ostrovsky <yurkasoft@gmail.com>
2021-11-06 10:17:04 +0200

    add BottomNavigator

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+function PhotosScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </SafeAreaView>
+  );
+}
+
+function FavoritesScreen() {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </SafeAreaView>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+
 
 export default function App() {
+ 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      
+    
+    <StatusBar style="auto" />
+    <NavigationContainer>
+    <Tab.Navigator >
+      <Tab.Screen name="Photos" component={PhotosScreen} />
+      <Tab.Screen name="Favorites" component={FavoritesScreen} />
+    </Tab.Navigator>
+    </NavigationContainer>
+    </SafeAreaView>
+ 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'red',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+ 
+  
+  
 });
+
+
